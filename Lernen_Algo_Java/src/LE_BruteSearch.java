014c0543dd4cdef85dc2282f504a014a72c606ad
lukas <lukas@DESKTOP-ERFTLIS.local>
2018-06-08 13:33:34 +0200

    BruteSearch

---
+++ Lernen_Algo_Java/src/LE_BruteSearch.java
@@ -0,0 +1,32 @@
+
+public class LE_BruteSearch {
+	
+	public static void bruteSearch(String text, String word){
+		for(int i = 0;i<text.length();i++){
+			
+			for(int j = 0; j<word.length();j++){
+				if(text.charAt(i) == word.charAt(j)){
+					System.out.println(text.charAt(i)+";"+word.charAt(j)+" ->Richtig!");
+					i++;
+				}else{
+
+					System.out.println(text.charAt(i)+";"+word.charAt(j)+" ->Falsch!");
+					break;
+				}
+			}
+		}
+	}
+
+	
+	public static void main(String []args){
+		
+		String text = "gegen gehen die gegend";
+		String word = "gegend";
+		
+		bruteSearch(text,word);
+
+	}
+	
+	
+
+}
